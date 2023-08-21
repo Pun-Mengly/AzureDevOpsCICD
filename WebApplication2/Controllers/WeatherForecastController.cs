7ec15bd2da83301b05020cdbe635e0f6ecd1f9b5
Mengly Pun <menglymmx@gmial.com>
2023-08-21 15:06:04 +0700

    dd

--- WebApplication2/Controllers/WeatherForecastController.cs
+++ WebApplication2/Controllers/WeatherForecastController.cs
@@ -21,7 +21,7 @@ namespace WebApplication2.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1,2).Select(index => new WeatherForecast
+            return Enumerable.Range(1,20).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
